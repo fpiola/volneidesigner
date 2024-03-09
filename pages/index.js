d60e80e0a273222f020705bec769eed47f45714d
fpiola <francis.piola@yahoo.com.br>
2024-03-09 17:55:35 -0300

    commit 8

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,12 @@ export default function Home() {
     <div className="container">
 
       <Head>
-        <head>
-          <title>{data.head.title}</title>
-          <meta name="keywords" content={data.head.keywords} />
-          <meta name="author" content="" />
-          <meta name="description" content={data.head.description} />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-        </head>
+        <title>{data.head.title}</title>
+        <meta name="keywords" content={data.head.keywords} />
+        <meta name="author" content="" />
+        <meta name="description" content={data.head.description} />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
       </Head>
 
       <header>
