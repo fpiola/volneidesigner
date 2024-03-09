207c0365d7388dd7af556f91ee470bf9525a1ed9
fpiola <francis.piola@yahoo.com.br>
2024-03-09 17:53:48 -0300

    head

--- pages/links.js
+++ pages/links.js
@@ -6,13 +6,16 @@ export default function Links() {
   return (
     <div className="container">
       <Head>
-        <title>{data.head.title}</title>
-        <meta name="keywords" content={data.head.keywords} />
-        <meta name="author" content="" />
-        <meta name="description" content={data.head.description} />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+        <head>
+          <title>{data.head.title}</title>
+          <meta name="keywords" content={data.head.keywords} />
+          <meta name="author" content="" />
+          <meta name="description" content={data.head.description} />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+        </head>
       </Head>
+
 
       <header>
         <Image src={data.profileImage.src} width={data.profileImage.width} height={data.profileImage.height} loading="lazy" className='logo' alt={data.profileImage.alt} />
